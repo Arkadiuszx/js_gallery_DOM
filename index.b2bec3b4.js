c2eefea01c467360b187d5be0fb5f9712151549e
Arkadiusz <stefanski.a00@gmail.com>
2024-02-03 09:42:59 +0100

    make build

---
+++ index.b2bec3b4.js
@@ -0,0 +1,4 @@
+var e=document.getElementById("largeImg");// eslint-disable-next-line no-shadow
+document.getElementById("thumbs").addEventListener("click",function(t){t.preventDefault();var n=t.target;e.src=n.parentElement.href});//# sourceMappingURL=index.b2bec3b4.js.map
+
+//# sourceMappingURL=index.b2bec3b4.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,11 @@
+"use strict";
+const mainImg = document.getElementById("largeImg");
+const list = document.getElementById("thumbs");
+// eslint-disable-next-line no-shadow
+list.addEventListener("click", (event)=>{
+    event.preventDefault();
+    const item = event.target;
+    mainImg.src = item.parentElement.href;
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
